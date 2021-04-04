b8159f7b9faf953f011eca365f12aa395c7ea815
Dan Digby <dandigby@me.com>
2021-04-04 12:31:16 +0100

    Adding reduce method to total movements

--- main.js
+++ main.js
@@ -91,6 +91,13 @@ const createUserNames = function(accs){
 
 createUserNames(accounts);
 
+const calcDisplayBalance = function(movements){
+  const balance = movements.reduce((acc, mov) => acc+mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+}
+
+calcDisplayBalance(account1.movements);
+
 
 
 /////////////////////////////////////////////////
